e666e168ea591b8f958e782d3aaf260372dacda2
yPatrick75 <pyumbillo75@gmail.com>
2019-09-14 09:07:32 -0500

    2 . Components

---
+++ src/components/Badge.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Badge extends React.Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <img src="" alt="" />
+                </div>
+            </div>
+        );
+    }
+}
+export default Badge;
--- src/index.js
+++ src/index.js
@@ -5,12 +5,13 @@
 
 // container.appendChild(element);
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import Badge from "./components/Badge";
 
 const element = <h1>Hello, Platzi Badges!</h1>;
 
-const container = document.getElementById('app');
+const container = document.getElementById("app");
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(<Badge />, container);
